9436b6caaf993a6888f4a85e9d12b5e0a402cd06
Kutman Sulaimanov <kutman.sulaimanov@alatoo.edu.kg>
2025-04-21 16:33:35 +0600

    Initial commit: JavaFX project with PostgreSQL DB connection

---
+++ src/main/java/com/project/db/Database.java
@@ -0,0 +1,32 @@
+package com.project.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private static final String URL = "jdbc:postgresql://localhost:5432/OnlineTestingPlatform_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "12345";
+
+    private static Connection connection;
+
+    private Database() {}
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module com.project.testing_platform {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires java.sql;
+
+    opens com.project.app to javafx.fxml;
+    exports com.project.app;
+    exports com.project.controller;
+    opens com.project.controller to javafx.fxml;
+}
